4cc20ea7a34fddaf3dce559d51a5a074747225d4
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-11-08 15:29:56 +0100

    ADD MapStreet inside index.vue

---
+++ app/components/CrossRoad/MapStreet.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container-map-street">
+    <div>
+      <p>test border</p>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-map-street {
+  border: black solid 3px;
+  border-radius: 0.5rem;
+  width: 100%;
+  height: auto;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
+import MapStreet from "~/components/CrossRoad/MapStreet.vue";
 </script>
 
 <template>
   <h1>Home Page</h1>
+  <MapStreet/>
 </template>
 
 <style scoped>
