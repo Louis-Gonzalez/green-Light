84c9385486a33721b010c967678144f1c1b8c491
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-11-08 17:57:42 +0100

    Refacto

--- app/components/CrossRoad/MapStreet.vue
+++ app/components/CrossRoad/MapStreet.vue
@@ -1,130 +1,103 @@
 <script setup lang="ts">
 import MapTile from "~/components/CrossRoad/CrossRoadCard/MapTile.vue";
 
-const firstLine = {
-  card1 : {
-    type: 'building'
+const data = {
+  firstLine : {
+    card1 : {
+      type: 'building'
+    },
+    card2 : {
+      type: 'building'
+    },
+    card3: {
+      type : 'road'
+    },
+    card4: {
+      type : 'building'
+    },
+    card5: {
+      type : 'building'
+    }
   },
-  card2 : {
-    type: 'building'
+  secondLine :{
+    card1 : {
+      type: 'building'
+    },
+    card2 : {
+      type: 'traffic light'
+    },
+    card3: {
+      type : 'road'
+    },
+    card4: {
+      type : 'traffic light'
+    },
+    card5: {
+      type : 'building'
+    }
   },
-  card3: {
-    type : 'road'
+  thirdLine : {
+    card1 : {
+      type: 'road'
+    },
+    card2 : {
+      type: 'road'
+    },
+    card3: {
+      type : 'road'
+    },
+    card4: {
+      type : 'road'
+    },
+    card5: {
+      type : 'road'
+    }
   },
-  card4: {
-    type : 'building'
+  fourthLine: {
+    card1 : {
+      type: 'building'
+    },
+    card2 : {
+      type: 'traffic light'
+    },
+    card3: {
+      type : 'road'
+    },
+    card4: {
+      type : 'traffic light'
+    },
+    card5: {
+      type : 'building'
+    }
   },
-  card5: {
-    type : 'building'
-  }
-}
-
-const secondLine = {
-  card1 : {
-    type: 'building'
-  },
-  card2 : {
-    type: 'traffic light'
-  },
-  card3: {
-    type : 'road'
-  },
-  card4: {
-    type : 'traffic light'
-  },
-  card5: {
-    type : 'building'
-  }
-}
-
-const thirdLine = {
-  card1 : {
-    type: 'road'
-  },
-  card2 : {
-    type: 'road'
-  },
-  card3: {
-    type : 'road'
-  },
-  card4: {
-    type : 'road'
-  },
-  card5: {
-    type : 'road'
-  }
-}
-
-const fourthLine = {
-  card1 : {
-    type: 'building'
-  },
-  card2 : {
-    type: 'traffic light'
-  },
-  card3: {
-    type : 'road'
-  },
-  card4: {
-    type : 'traffic light'
-  },
-  card5: {
-    type : 'building'
-  }
-}
-const fifthLine = {
-  card1 : {
-    type: 'building'
-  },
-  card2 : {
-    type: 'building'
-  },
-  card3: {
-    type : 'road'
-  },
-  card4: {
-    type : 'building'
-  },
-  card5: {
-    type : 'building'
+  fifthLine: {
+    card1 : {
+      type: 'building'
+    },
+    card2 : {
+      type: 'building'
+    },
+    card3: {
+      type : 'road'
+    },
+    card4: {
+      type : 'building'
+    },
+    card5: {
+      type : 'building'
+    }
   }
 }
 </script>
 
 <template>
   <div class="container-map-street">
-    <div v-for="card in firstLine" key="index">
-      <MapTile
-          :type="card.type"
-      />
-    </div>
-  </div>
-  <div class="container-map-street">
-    <div v-for="card in secondLine" key="index">
-      <MapTile
-          :type="card.type"
-      />
-    </div>
-  </div>
-  <div class="container-map-street">
-    <div v-for="card in thirdLine" key="index">
-      <MapTile
-          :type="card.type"
-      />
-    </div>
-  </div>
-  <div class="container-map-street">
-    <div v-for="card in fourthLine" key="index">
-      <MapTile
-          :type="card.type"
-      />
-    </div>
-  </div>
-  <div class="container-map-street">
-    <div v-for="card in fifthLine" key="index">
-      <MapTile
-          :type="card.type"
-      />
+    <div v-for="item in data" key="index">
+      <div v-for="card in item" :key="index">
+        <MapTile
+            :type="card.type"
+        />
+      </div>
     </div>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-
 import MapStreet from "~/components/CrossRoad/MapStreet.vue";
 </script>
 
 <template>
-  <h1>Home Page</h1>
   <MapStreet/>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
